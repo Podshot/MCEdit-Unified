2c54186acbac89ec3bff46e3b0c82df85455b30a
Podshot <ben.gothard3@gmail.com>
2014-09-26 09:52:08 -0400

    Added an example file of how I will document the code

---
+++ doc/documentaion_layout.py
@@ -0,0 +1,31 @@
+'''
+This python file is not accual code, it is just a layout of how I will
+document MCEdit.
+Documentaion tags:
+@param ____: A parameter to a function. '____' is the parameter name and
+what the parameter should be (Like an int, string, level object, or albow widget)
+@return: What the function returns (Like a boolean, string, or exception
+@raises ____: What error the function could throw
+@what-it-does: What the function accomplishes
+'''
+
+'''
+@param arg: Argument to print to console
+@what-it-does: Prints the argument to console 
+'''
+def paramFunction(arg):
+    print "Arg: "+str(arg)
+
+'''
+@raises Exception: This function should always raise a generic Exception
+@what-it-does: Raises a generic Exception
+'''
+def throwsError():
+    raise Exception()
+
+'''
+@return: Returns a boolean with the value of True
+@what-it-does: Returns True
+'''
+def returnFunction():
+    return True
