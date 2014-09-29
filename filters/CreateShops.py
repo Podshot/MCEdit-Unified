55b0818e3778dfe1875441018706aae8956b8d2b
Khroki <Chrisstaples4@gmail.com>
2014-09-28 22:54:47 -0400

    TrinTrevellion's create shops filter update
    for 1.8

--- filters/CreateShops.py
+++ filters/CreateShops.py
@@ -1,6 +1,15 @@
 # Feel free to modify and use this filter however you wish. If you do,
 # please give credit to SethBling.
 # http://youtube.com/SethBling
+#
+# Fixed for MC 1.8 by Rezzing, September 6, 2014
+#
+# PLEASE READ: The unusable trade won't prevent new trades anymore but I
+# solved it by adding a very high career level. So only use it if you like
+# the fancy stop sign. This traders won't walk away anymore. Their speed
+# is set to zero by "movement["Base"] = TAG_Double(0)". The default value
+# would be 0.5 or something. You can rename them at ["CustomName"].
+# And big thanks to SethBling for this awesome MCEdit filter :)
 
 from pymclevel import TAG_Compound
 from pymclevel import TAG_Int
@@ -65,6 +74,8 @@ def createShop(level, x, y, z, emptyTrade, invincible, profession, unlimited):
 			saleList[slot-18] = item
 
 	villager = TAG_Compound()
+	villager["CustomNameVisible"] = TAG_Byte(0)
+	villager["Invulnerable"] = TAG_Byte(1)
 	villager["OnGround"] = TAG_Byte(1)
 	villager["Air"] = TAG_Short(300)
 	villager["AttackTime"] = TAG_Short(0)
@@ -73,9 +84,12 @@ def createShop(level, x, y, z, emptyTrade, invincible, profession, unlimited):
 	villager["Health"] = TAG_Short(20)
 	villager["HurtTime"] = TAG_Short(0)
 	villager["Age"] = TAG_Int(0)
+	villager["Career"] = TAG_Int(1)
+	villager["CareerLevel"] = TAG_Int(1000)
 	villager["Profession"] = TAG_Int(profession)
-	villager["Riches"] = TAG_Int(0)
+	villager["Riches"] = TAG_Int(200)
 	villager["FallDistance"] = TAG_Float(0)
+	villager["CustomName"] = TAG_String("Trader")
 	villager["id"] = TAG_String("Villager")
 	villager["Motion"] = TAG_List()
 	villager["Motion"].append(TAG_Double(0))
@@ -95,6 +109,8 @@ def createShop(level, x, y, z, emptyTrade, invincible, profession, unlimited):
 		if (i in priceList or i in priceListB) and i in saleList:
 			offer = TAG_Compound()
 			if unlimited:
+				offer["rewardExp"] = TAG_Byte(0)
+				offer["maxUses"] = TAG_Int(1000)
 				offer["uses"] = TAG_Int(-2000000000)
 			else:
 				offer["uses"] = TAG_Int(0)
@@ -110,16 +126,22 @@ def createShop(level, x, y, z, emptyTrade, invincible, profession, unlimited):
 			offer["sell"] = saleList[i]
 			villager["Offers"]["Recipes"].append(offer)
 
+		villager["Attributes"] = TAG_List()
+		movement = TAG_Compound()
+		movement["Base"] = TAG_Double(0)
+		movement["Name"] = TAG_String("generic.movementSpeed")
+		villager["Attributes"].append(movement)
+
 	if emptyTrade:
 		offer = TAG_Compound()
 		offer["buy"] = TAG_Compound()
 		offer["buy"]["Count"] = TAG_Byte(1)
 		offer["buy"]["Damage"] = TAG_Short(0)
-		offer["buy"]["id"] = TAG_Short(36)
+		offer["buy"]["id"] = TAG_Short(166)
 		offer["sell"] = TAG_Compound()
 		offer["sell"]["Count"] = TAG_Byte(1)
 		offer["sell"]["Damage"] = TAG_Short(0)
-		offer["sell"]["id"] = TAG_Short(36)
+		offer["sell"]["id"] = TAG_Short(166)
 		villager["Offers"]["Recipes"].append(offer)
 
 	if invincible:
