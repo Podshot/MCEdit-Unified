233294e3f11d499b961f0a523663d03c9184ea0b
Podshot <ben.gothard3@gmail.com>
2014-10-04 18:53:01 -0400

    Started work on new version checking

--- release.py
+++ release.py
@@ -1,6 +1,8 @@
 import os.path
 import subprocess
 import directories
+import json
+import HTMLParser
 
 
 def get_version():
@@ -19,6 +21,14 @@ def get_version():
 
     return v
 
+def new_get_current_version():
+    current = json.load(open(os.path.join(directories.dataDir, "RELEASE-VERSION-JSON"), 'rb'))
+    return current["name"]
+
+def new_get_current_commit():
+    current = json.load(open(os.path.join(directories.dataDir, "RELEASE-VERSION-JSON"), 'rb'))
+    return current["commit"]
+
 
 def get_commit():
     """
@@ -36,6 +46,7 @@ def get_commit():
 
     return v
 
-
+print new_get_current_version()
+print new_get_current_commit()
 release = get_version()
 commit = get_commit()
