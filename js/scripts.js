02367c41fe235ddb8316fdebc2a74aad4145c983
Ezekiel Elin <ezekielelin@me.com>
2014-10-05 10:04:11 -0400

    Added nabber buttons, fixed download link

--- js/scripts.js
+++ js/scripts.js
@@ -101,7 +101,19 @@ function generatePageStructure() {
 		}
 		$('#navbar').append('<li class="' + (active ? 'active' : '') + '"><a href="' + navjson.root + navitem.url + '">' + navitem.displayname + '</a></li>')
 	}
-	$('#navbar').after('<ul class="nav navbar-nav navbar-right"><p class="navbar-text navbar-right">Currently v' + releaseData[0].tag_name + ' (' + new Date(releaseData[0].created_at).toLocaleDateString() + ')</p></li>');
+	$('#navbar').after('<form class="navbar-form navbar-right"></form>');
+	latestRelease = releaseData[0];
+	for (var i = 0; i < platforms.length; i++) {
+		var asset32 = getDownload(platforms[i],latestRelease.tag_name,32);
+		var asset64 = getDownload(platforms[i],latestRelease.tag_name,64);
+		if (asset32) {
+			console.log(asset32);
+			$('.navbar-form.navbar-right').append('<a class="btn btn-default" href="' + asset32.browser_download_url + '">Download <i class="fa fa-' + (platforms[i] == 'OSX' ? 'apple' : 'windows') + '"></i> ' + platforms[i] + ' 32bit</a> ');
+		}
+		if (asset64) {
+			$('.navbar-form.navbar-right').append('<a class="btn btn-default" href="' + asset64.browser_download_url + '">Download <i class="fa fa-' + (platforms[i] == 'OSX' ? 'apple' : 'windows') + '"></i> ' + platforms[i] + ' 64bit</a> ');
+		}
+	}	
 	return true;
 }
 function getDownload(platform,version,bittage) {
