eb6fc9b3032609501ce9e539acaa5532c43d1231
naor2013 <naor2013@icloud.com>
2014-10-18 19:57:22 +0300

    Trying to figure out the problem

--- pymclevel/entity.py
+++ pymclevel/entity.py
@@ -162,6 +162,7 @@ class TileEntity(object):
                         pos = Entity.pos(mob)
                         x, y, z = [str(part) for part in pos]
                         x, y, z = coords(x, y, z, moveSpawnerPos)
+                        print x,y,z
                         mob['Temp1'] = x
                         mob['Temp2'] = y
                         mob['Temp3'] = z
