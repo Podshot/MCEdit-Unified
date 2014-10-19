d20d499b1eb4681f3961fc59f92c8397d1d2d819
Khroki <Chrisstaples4@gmail.com>
2014-10-19 16:11:06 -0400

    New batch files

--- mcedit-debug.bat
+++ mcedit-debug.bat
@@ -1,2 +1,20 @@
+@echo off
+SET STATUS=,
+:START:
 python.exe mcedit.py
-pause
+ECHO.
+ECHO ^|-----------------^|
+ECHO ^|Mcedit Terminated^|
+ECHO ^|-----------------^|
+ECHO.
+SET /P K=Press Y to restart MCEdit%STATUS% any other key to exit:
+IF /I %K%==Y GOTO CLEAR
+
+GOTO EXIT
+
+:CLEAR:
+CLS
+SET STATUS= again,
+GOTO START
+
+:EXIT:
