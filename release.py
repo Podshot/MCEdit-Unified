0251cdfd062aa8a8f3fd09146b08c78cc4ebf273
Ezekiel Elin <ezekielelin@me.com>
2014-10-26 21:00:52 -0400

    Updated release system Renamed release-version file, added a "developement" attribute. Does not check for updates if you're running a "developement" build.

--- release.py
+++ release.py
@@ -8,7 +8,7 @@ from sys import platform as _platform
 
 def get_version():
     try:
-        with open(os.path.join(directories.getDataDir(), "RELEASE-VERSION"), 'rb') as jsonString:
+        with open(os.path.join(directories.getDataDir(), "RELEASE-VERSION.json"), 'rb') as jsonString:
             current = json.load(jsonString)
             return current["name"].replace("{tag_name}",current["tag_name"])
     except:
@@ -16,38 +16,47 @@ def get_version():
 
 def get_release_tag():
     try:
-        with open(os.path.join(directories.getDataDir(), "RELEASE-VERSION"), 'rb') as jsonString:
+        with open(os.path.join(directories.getDataDir(), "RELEASE-VERSION.json"), 'rb') as jsonString:
             current = json.load(jsonString)
             return current["tag_name"]
     except:
         raise
-
+def is_dev():
+    try:
+        with open(os.path.join(directories.getDataDir(), "RELEASE-VERSION.json"), 'rb') as jsonString:
+            current = json.load(jsonString)
+            return current["developement"]
+    except:
+        raise
 def check_for_new_version():
     try:
-        release_api_response = json.loads(urllib2.urlopen("https://api.github.com/repos/Khroki/MCEdit-Unified/releases").read())
-        version = release_api_response[0]
-        if version["tag_name"] != get_release_tag():
-            is_64bit = sys.maxsize > 2**32
-            assets = version["assets"]
-            for asset in assets:
-                if _platform == "win32":
-                    version["OS Target"] = "windows"
-                    if "Win" in asset["name"]:
-                        if is_64bit:
-                            if "64bit" in asset["name"]:
-                                version["asset"] = asset
-                                version["target_arch"] = "64bit"
-                        else:
-                            if "32bit" in asset["name"]:
-                                version["asset"] = asset
-                                version["target_arch"] = "32bit"
-                elif _platform == "darwin":
-                    version["OS Target"] = "osx"
-                    if "OSX" in asset["name"]:
-                        version["asset"] = asset
-                        version["target_arch"] = "64bit"
-            return version
-        return False
+        if not is_dev():
+            release_api_response = json.loads(urllib2.urlopen("https://api.github.com/repos/Khroki/MCEdit-Unified/releases").read())
+            version = release_api_response[0]
+            if version["tag_name"] != get_release_tag():
+                is_64bit = sys.maxsize > 2**32
+                assets = version["assets"]
+                for asset in assets:
+                    if _platform == "win32":
+                        version["OS Target"] = "windows"
+                        if "Win" in asset["name"]:
+                            if is_64bit:
+                                if "64bit" in asset["name"]:
+                                    version["asset"] = asset
+                                    version["target_arch"] = "64bit"
+                            else:
+                                if "32bit" in asset["name"]:
+                                    version["asset"] = asset
+                                    version["target_arch"] = "32bit"
+                    elif _platform == "darwin":
+                        version["OS Target"] = "osx"
+                        if "OSX" in asset["name"]:
+                            version["asset"] = asset
+                            version["target_arch"] = "64bit"
+                return version
+            return False
+        else:
+            return False
     except:
         print "An error occurred checking for updates."
         return False
