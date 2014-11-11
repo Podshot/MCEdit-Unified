f5b814273feeb906d9e0fe69868278f7e2c19f60
naor2013 <naor2013@icloud.com>
2014-11-11 22:51:54 +0200

    Another fix

--- mceutils.py
+++ mceutils.py
@@ -639,7 +639,9 @@ def showProgress(progressText, progressIterator, cancel=False):
             try:
                 if "SelectionTool" in "{0}".format(self.get_root().mcedit.editor.currentTool):
                     if self.get_root().mcedit.editor.currentTool.panel.nudgeBlocksButton.count > 0:
-                        self.get_root().mcedit.editor.currentTool.panel.nudgeBlocksButton.mouse_up(event) 
+                        self.get_root().mcedit.editor.currentTool.panel.nudgeBlocksButton.mouse_up(event)
+            except:
+                pass 
 
     widget = ProgressWidget()
     widget.progressText = _(progressText)
