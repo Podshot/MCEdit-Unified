b2fedbcfefc1a2f991bf33a31a45e1dee76c3399
freundTech <freundTech@gmail.com>
2014-11-14 15:07:58 +0100

    Some more work on the inventory editor

--- editortools/player.py
+++ editortools/player.py
@@ -20,6 +20,7 @@ from albow.translate import _
 import config
 from editortools.editortool import EditorTool
 from editortools.tooloptions import ToolOptions
+from editortools.playereditor import Inventory
 from glbackground import Panel
 from glutils import DisplayList
 from mceutils import loadPNGTexture, alertException, drawTerrainCuttingWire, drawCube
@@ -171,16 +172,18 @@ class PlayerAddOperation(Operation):
 
         self.tool.markerList.invalidate()
 
-class PlayerAddOperation(Operation):
+class PlayerEditOperation(Operation):
     undoTag = None
 
     def __init__(self, tool, pos, player="Player"):
-        super(PlayerAddOperation, self).__init__(tool.editor, tool.editor.level)
+        super(PlayerEditOperation, self).__init__(tool.editor, tool.editor.level)
         self.tool = tool
         self.level = self.tool.editor.level
+        self.player = player
 
     def perform(self, recordUndo=True):
-        pass
+        inv = Inventory(self.player, self.level.getPlayerTag(self.player))
+        inv.present()
 
     def undo(self):
         pass
@@ -356,7 +359,7 @@ class PlayerPositionPanel(Panel):
         gotoCameraButton = Button("Goto Player's View", action=self.tool.gotoPlayerCamera)
         moveButton = Button("Move Player", action=self.tool.movePlayer)
         moveToCameraButton = Button("Align Player to Camera", action=self.tool.movePlayerToCamera)
-        col.extend([addButton, removeButton, gotoButton, gotoCameraButton, moveButton, moveToCameraButton])
+        col.extend([addButton, removeButton, editButton, gotoButton, gotoCameraButton, moveButton, moveToCameraButton])
 
         col = Column(col)
         self.add(col)
