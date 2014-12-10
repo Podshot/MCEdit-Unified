ff296661cca99c31d4c8c647f9811e01ad725caf
naor2013 <naor2013@icloud.com>
2014-12-10 21:42:28 +0200

    Maybe now

--- leveleditor.py
+++ leveleditor.py
@@ -2740,7 +2740,7 @@ class LevelEditor(GLViewport):
         if self.renderer.needsImmediateRedraw:
             self.invalidate()
 
-        if self.root.bonus_draw_time < self.renderer.viewDistance*3:
+        if self.root.bonus_draw_time < self.renderer.viewDistance:
             frameDuration = self.getFrameDuration()
 
             while frameDuration > (datetime.now() - self.frameStartTime):
