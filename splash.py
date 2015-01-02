7dec61e62f1166dc2b60a9278350dfd9c4fe3f17
LaChal <denis.cuinat-guerraz@laposte.net>
2015-01-02 21:40:52 +0100

    Splashscreen code credits...

--- splash.py
+++ splash.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+# Taken from http://www.pygame.org/project-Splash+screen-1186-.html by Rock Achu (rockhachu2)
+# and tweaked ;)
 import pygame
 import time
 import os, sys
@@ -8,11 +10,7 @@ pygame.init()
 pygame.font.init()
 splash = pygame.image.load("splash.png")
 screen = pygame.display.set_mode(splash.get_size(),pygame.NOFRAME)
-background = pygame.Surface(screen.get_size())
-background.fill((2,24,244))
-#screen.blit(background, (0,0))
-#screen.blit(pygame.font.Font('pala.ttf', 72).render('Loading...', 1, (255,255,255)), (90,10))
 screen.blit(splash, (0,0))
 pygame.display.update()
 os.environ['SDL_VIDEO_CENTERED'] = '0'
-# time.sleep(5)
+
