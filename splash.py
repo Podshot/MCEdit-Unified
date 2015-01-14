58f2b2df327bd04bf6cd3de80c5d5af79c558eee
LaChal <denis.cuinat-guerraz@laposte.net>
2015-01-14 23:09:39 +0100

    Fix for splash screen crash on Windows with .exe form.

--- splash.py
+++ splash.py
@@ -7,9 +7,19 @@ print 'Splash load...'
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
 pygame.font.init()
-splash = pygame.image.load("splash.png")
-screen = pygame.display.set_mode(splash.get_size(),pygame.NOFRAME)
-screen.blit(splash, (0,0))
+try:
+    try:
+        splash = pygame.image.load("splash.png")
+    except:
+        splash = pygame.image.load(os.path.join(".", "splash.png"))
+    screen = pygame.display.set_mode(splash.get_size(),pygame.NOFRAME)
+    screen.blit(splash, (0,0))
+except:
+    font = pygame.font.Font(pygame.font.get_default_font(), 48)
+    buf = font.render("MCEDit is loading...", True, (128, 128, 128))
+    screen = pygame.display.set_mode((buf.get_width() + 20, buf.get_height() + 20), pygame.NOFRAME)
+    screen.blit(buf, (10, 10))
+    splash = pygame.display.get_surface()
 pygame.display.update()
 os.environ['SDL_VIDEO_CENTERED'] = '0'
 
