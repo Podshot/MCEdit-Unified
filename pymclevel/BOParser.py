b0f38be855c9024a9d05a3a44b08e7d32c398886
Podshot <ben.gothard3@gmail.com>
2015-01-29 08:24:54 -0500

    Trying to convert BO2 and BO3 formats to schematic

--- pymclevel/BOParser.py
+++ pymclevel/BOParser.py
@@ -1,6 +1,7 @@
 import yaml
 import ConfigParser
 import os
+from pymclevel import schematic
 
 class BO3:
     
@@ -21,13 +22,19 @@ class BO2:
         self.__meta = {}
         self.__blocks = {}
         if filename != '':
+            self.__schem = schematic.MCSchematic(shape=(1,1,1))
             self._parser.read(filename)
             self.__version = self._parser.get('META', 'version')
             for item in self._parser.items("META"):
                 self.__meta[item[0]] = item[1]
                 
             for block in self._parser.items("DATA"):
-                self.__blocks[block[0]] = block[1]
+                #self.__blocks[block[0]] = block[1]
+                coords = block[0].split(",")
+                x = int(coords[0])
+                y = int(coords[1])
+                z = int(coords[2])
+                print x,y,z
                 
             # Format is Y,X,Z where 'Z' is elevation. WHO DECIDED THAT?!?
             print self.__blocks
@@ -47,4 +54,4 @@ def parse_bo2(bo2_file):
 def save_bo2(bo2_file):
     pass
 
-BO2(filename="test"+os.path.sep+'Asteroid1.BO2')
+BO2(filename="pymclevel"+os.path.sep+"test"+os.path.sep+'Asteroid1.BO2')
