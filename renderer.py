04a5acdaf91e45e5837a1567499cad3a48160421
naor2013 <naor2013@icloud.com>
2015-02-25 14:36:31 +0200

    Reverted changes that cause a weird bug

--- renderer.py
+++ renderer.py
@@ -2985,39 +2985,40 @@ class MCRenderer(object):
             GL.glMatrixMode(GL.GL_TEXTURE)
             GL.glScalef(1 / 2., 1 / 2., 1 / 2.)
 
-        dx, dy, dz = self.origin
-        GL.glTranslate(dx, dy, dz)
+        with gl.glPushMatrix(GL.GL_MODELVIEW):
+            dx, dy, dz = self.origin
+            GL.glTranslate(dx, dy, dz)
 
-        GL.glEnable(GL.GL_CULL_FACE)
-        GL.glEnable(GL.GL_DEPTH_TEST)
-        GL.glEnable(GL.GL_TEXTURE_2D)
-        GL.glEnableClientState(GL.GL_TEXTURE_COORD_ARRAY)
-            
-        self.level.materials.terrainTexture.bind()
+            GL.glEnable(GL.GL_CULL_FACE)
+            GL.glEnable(GL.GL_DEPTH_TEST)
 
-        offset = DepthOffset.PreviewRenderer if self.isPreviewer else DepthOffset.Renderer
-        GL.glPolygonOffset(offset, offset)
-        GL.glEnable(GL.GL_POLYGON_OFFSET_FILL)
+            self.level.materials.terrainTexture.bind()
+            GL.glEnable(GL.GL_TEXTURE_2D)
+            GL.glEnableClientState(GL.GL_TEXTURE_COORD_ARRAY)
 
-        self.createMasterLists()
-        try:
-            self.callMasterLists()
+            offset = DepthOffset.PreviewRenderer if self.isPreviewer else DepthOffset.Renderer
+            GL.glPolygonOffset(offset, offset)
+            GL.glEnable(GL.GL_POLYGON_OFFSET_FILL)
 
-        except GL.GLError, e:
-            if self.errorLimit:
-                self.errorLimit -= 1
-                traceback.print_exc()
-                print e
+            self.createMasterLists()
+            try:
+                self.callMasterLists()
 
-        GL.glDisable(GL.GL_POLYGON_OFFSET_FILL)
+            except GL.GLError, e:
+                if self.errorLimit:
+                    self.errorLimit -= 1
+                    traceback.print_exc()
+                    print e
 
-        GL.glDisable(GL.GL_CULL_FACE)
-        GL.glDisable(GL.GL_DEPTH_TEST)
+            GL.glDisable(GL.GL_POLYGON_OFFSET_FILL)
 
-        GL.glDisable(GL.GL_TEXTURE_2D)
-        GL.glDisableClientState(GL.GL_TEXTURE_COORD_ARRAY)
-            
-        self.drawLoadableChunkMarkers()
+            GL.glDisable(GL.GL_CULL_FACE)
+            GL.glDisable(GL.GL_DEPTH_TEST)
+
+            GL.glDisable(GL.GL_TEXTURE_2D)
+            GL.glDisableClientState(GL.GL_TEXTURE_COORD_ARRAY)
+            # if self.drawLighting:
+            self.drawLoadableChunkMarkers()
 
         if self.level.materials.name in ("Pocket", "Alpha"):
             GL.glMatrixMode(GL.GL_TEXTURE)
