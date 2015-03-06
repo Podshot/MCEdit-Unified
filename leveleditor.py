a06f3fca04065785f23c010eb4eaf471f743be7d
naor2013 <naor2013@icloud.com>
2015-03-06 20:28:43 +0200

    Maybe a fix

--- leveleditor.py
+++ leveleditor.py
@@ -1159,8 +1159,9 @@ class LevelEditor(GLViewport):
 
     @mceutils.alertException
     def saveAs(self):
-        filename = mcplatform.askSaveFile(directories.minecraftSaveFileDir, "Name the new copy.",
-                                          "Copy of " + self.level.worldFolder.filename, _('Minecraft World\0*.*\0\0'), "")
+        shortName = os.path.split(os.path.split(self.level.filename)[0])[1]
+        filename = mcplatform.askSaveFile(directories.minecraftSaveFileDir, _("Name the new copy."),
+                                          "Copy of " + shortName, _('Minecraft World\0*.*\0\0'), "")
         if filename is None:
             return
         shutil.copytree(self.level.worldFolder.filename, filename)
