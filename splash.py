ddcac372a68302476b9fb1c2d28a2ecd02c0eb47
LaChal <denis.cuinat-guerraz@laposte.net>
2015-03-07 13:32:18 +0100

    Added random splash screen support.

--- splash.py
+++ splash.py
@@ -7,6 +7,7 @@ print 'Splash load...'
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
 pygame.font.init()
+no_splash = False
 try:
     try:
         splash = pygame.image.load("splash.png")
@@ -20,6 +21,18 @@ except:
     screen = pygame.display.set_mode((buf.get_width() + 20, buf.get_height() + 20), pygame.NOFRAME)
     screen.blit(buf, (10, 10))
     splash = pygame.display.get_surface()
+    no_splash = True
 pygame.display.update()
 os.environ['SDL_VIDEO_CENTERED'] = '0'
+
+# Random splash
+if not no_splash:
+    from random import choice
+    from shutil import copyfile
+    splashes_folder = 'splashes'
+    if not os.path.exists(splashes_folder):
+        splashes_folder = os.path.join('.', splashes_folder)
+    if os.path.exists(splashes_folder):
+        new_splash = choice(os.listdir(splashes_folder))
+        copyfile(os.path.join(splashes_folder, new_splash), os.path.join('.', 'splash.png'))
 
