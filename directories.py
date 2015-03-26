e742efd12865b7fe0b4aead33f733467a2884f8f
Podshot <ben.gothard3@gmail.com>
2015-03-26 07:04:26 -0400

    Reverted directory changes

--- directories.py
+++ directories.py
@@ -72,10 +72,13 @@ def getDataDir():
             return x.decode(sys.getfilesystemencoding())
 
         argzero = fsdecode(os.path.abspath(sys.argv[0]))
+        dataDir = os.getcwdu()
+        '''
         if getattr(sys, 'frozen', False):
             dataDir = os.path.dirname(sys._MEIPASS)
         else:
             dataDir = os.path.dirname(__file__)
+        '''
             #dataDir = str(os.path.sep).join(dataDir.split(os.path.sep)[:-1])
             
     else:
