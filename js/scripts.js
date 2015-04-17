09d446494a3fe76960f995e3dc6cd416973a3774
Ezekiel Elin <ezekielelin@me.com>
2015-04-16 20:47:08 -0700

    Fixed rate limiting stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,10 @@
 var platforms = ["OSX","Win", "Lin"];
 var requests = {};
-var HAVE_I_BEEN_API_LIMITED_YET = false;
 function getJSON(url){
-	if (url.indexOf('api.github') != -1 && HAVE_I_BEEN_API_LIMITED_YET) {
-		$('title').html('MCEdit Unified - Rate Limit Exceeded');
-		$('body').html('<h1>Rate Limit Exceeded</h1><br>Click <a href="https://github.com/Khroki/MCEdit-Unified">here</a> to go to the repository page instead.');
-		$('body').css('background-color','#444444').css('text-align','center').css('color','white');
-		return {};
-	}
+	var ret_val = {};
 	if (requests[url]) {
 		console.log('Found cached version');
-		return requests[url];
+		ret_val = requests[url];
 	} else {
 		try {
 			var response = $.ajax({
@@ -22,7 +16,7 @@ function getJSON(url){
 			var ret_val = JSON.parse(response);
 			if (ret_val !== undefined) {
 				requests[url] = ret_val;
-				return requests[url];
+				ret_val = requests[url];
 			} else {
 				loadFailError();
 			}
@@ -31,6 +25,14 @@ function getJSON(url){
 			loadFailError();
 		}
 	}
+	if (ret_val && ret_val.message && ret_val.message.indexOf('API rate limit exceeded') != -1) {
+		$('title').html('MCEdit Unified - Rate Limit Exceeded');
+		$('body').children().not('nav').hide();
+		$('body').append('<div id="exceededwarning"><h1>Rate Limit Exceeded</h1><br>This page requires calls to GitHub, which is a rate limited resource.</div>');
+		$('body').css('background-color','#444444');
+		$('#exceededwarning').css('text-align','center').css('color','white');
+	}
+	return ret_val;
 }
 /*versionCompare from http://stackoverflow.com/a/6832721*/
 function compareVersionString(v1, v2, options) {
@@ -138,7 +140,7 @@ function getDownload(platform,version,bittage) {
 		var release = getReleaseData()[i];
 		for (var x = 0; x < release.assets.length; x++) {
 			var asset = release.assets[x];
-            var name = 'MCEdit.v' + version + '.' + platform + '.' + bittage + bitbit;
+			var name = 'MCEdit.v' + version + '.' + platform + '.' + bittage + bitbit;
 			if (asset.name == name + '.zip' || asset.name == name + '.exe' || asset.name == name + '.msi' || asset.name == name + '.run') {
 				return asset;
 			}
@@ -165,23 +167,19 @@ function getReleaseData() {
 }
 $(document).ready(function(){
 	var ratelimits = getJSON('https://api.github.com/rate_limit');
-	if (ratelimits.resources.core.remaining < 5) {
+	if (ratelimits.resources.core.remaining < 5 && ratelimits.resources.core.remaining > 0) {
 		$('body').children().hide();
 		$('body').append('<div id="ratewarning"><h1>Rate Limit Low</h1><br>You only have ' + ratelimits.resources.core.remaining + ' requests remaining<br><br><button onclick="$(\'#ratewarning\').remove();$(\'body\').css(\'background-color\',\'white\').children().show();" class="btn btn-default"><i class="fa fa-check"></i> Ok</button></div>');
 		$('body').css('background-color','#444444');
 		$('#ratewarning').css('text-align','center').css('color','white');
 	}
-	if (ratelimits.resources.core.remaining > 0) {
-		if (generatePageStructure()) {
-			try {
-				pageTrigger();
-			} catch(err) {
-				console.log(err.message);
-			}
-		} else {
-			alert('An error occured loading the webpage. Please try again later.');
+	if (generatePageStructure()) {
+		try {
+			pageTrigger();
+		} catch(err) {
+			console.log(err.message);
 		}
 	} else {
-		var HAVE_I_BEEN_API_LIMITED_YET = true;
+		alert('An error occured loading the webpage. Please try again later.');
 	}
 });
