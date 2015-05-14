2c8a44baa711471547e610f3616ffe76cbb0d0b8
LaChal <denis.cuinat-guerraz@laposte.net>
2015-05-14 19:28:04 +0200

    BO3 support: added support for RandomBlock() tag. The block is choosen on BO3 data load time.

--- pymclevel/BOParser.py
+++ pymclevel/BOParser.py
@@ -1,7 +1,11 @@
 import ConfigParser
 from pymclevel import schematic, materials
+from entity import TileEntity
+import nbt
 import logging
 import re
+import os
+from random import randint
 
 log = logging.getLogger(__name__)
 
@@ -94,6 +98,9 @@ class BO3:
     def getSchematic(self):
         return self.__schem
 '''
+# find athoer way for this.
+# keys are block ids in uppercase, values are tuples for ranges, lists for exact states
+corrected_states = {'CHEST':(2,6)}
 
 class BO3:
     def __init__(self, filename=''):
@@ -101,6 +108,8 @@ class BO3:
             self.delta_x, self.delta_y, self.delta_z = 0, 0, 0
             self.size_x, self.size_y, self.size_z = 0, 0, 0
             map_block = {}
+            not_found = []
+            tileentities_list = [a.lower() for a in TileEntity.baseStructures.keys()]
             for k, v in materials.block_map.items():
                 map_block[v.replace('minecraft:', '')] = k
 
@@ -122,23 +131,88 @@ class BO3:
             lines = re.findall(r'^Block\(.*?\)|^RandomBlock\(.*?\)', raw_data, re.M)
             [get_delta(*b) for b in [eval(','.join(a.split('(')[1].split(')')[0].split(',', 3)[:3])) for a in lines]]
             self.__schem = schematic.MCSchematic(shape=(self.size_x, self.size_y, self.size_z))
-            for line in lines:
-                if line.startswith('Block'):
-                    x, y, z, b = line.replace("Block(", "").replace(")","").strip().split(",")
-                    x = int(x) + self.delta_x
-                    y = int(y) + self.delta_y
-                    z = int(z) + self.delta_z
-                    b_id, b_state = (b + ':0').split(':')[:2]
-                    if b_state:
-                        b_state = int(b_state)
+
+            def get_block_data(args):
+                x, y, z = args[:3]
+                b = args[3]
+                nbt_data = None
+                if len(args) == 5 and args[4] != None:
+                    f_name = os.path.join(os.path.dirname(filename), os.path.normpath(args[4]))
+                    if os.path.exists(f_name):
+                        nbt_data = nbt.load(f_name)
                     else:
-                        b_state = 0
+                        print 'Could not find %s'%args[4]
+                        print '  Canonical path: %s'%f_name
+                x = int(x) + self.delta_x
+                y = int(y) + self.delta_y
+                z = int(z) + self.delta_z
+                b_id, b_state = (b + ':0').split(':')[:2]
+                if b_state:
+                    b_state = int(b_state)
+                else:
+                    b_state = 0
+                return x, y, z, b_id, b_state, nbt_data
+
+            def get_randomblock_data(args):
+                x, y, z = args[:3]
+                obj = []
+                bit_id = False
+                bit_path = False
+                bit_chance = False
+                for arg in args[3:]:
+                    if not bit_id:
+                        obj.append(arg)
+                        bit_id = True
+                    elif arg.isdigit():
+                        if not bit_path:
+                            obj.append(None)
+                            bit_path = True
+                        obj.append(int(arg))
+                        bit_chance = True
+                    else:
+                        obj.append(arg)
+                        bit_path = True
+                    if bit_id and bit_path and bit_chance:
+                        if randint(1, 100) <= obj[2]:
+                            break
+                        obj = []
+                        bit_id, bit_path, bit_chance = False, False, False
+                #print 'Selected random object: %s'%obj
+                return get_block_data((x, y, z, obj[0], obj[1]))
+
+            def verify_state(id, state):
+                states = corrected_states.get(id, None)
+                if states:
+                    if type(states) == tuple:
+                        if state not in range(*states):
+                            state = states[0]
+                    elif type(states) == list:
+                        if state not in states:
+                            state = states[0]
+                return state
+
+            for line in lines:
+                if line.startswith('Block') or line.startswith('RandomBlock'):
+                    if line.startswith('Block'):
+                        x, y, z, b_id, b_state, nbt_data = get_block_data(line.replace("Block(", "").replace(")","").strip().split(","))
+                    else:
+                        x, y, z, b_id, b_state, nbt_data = get_randomblock_data(line.replace("RandomBlock(", "").replace(")","").strip().split(","))
+
                     if map_block.get(b_id.lower(), None) != None:
-                        b_id = int(map_block[b_id.lower()])
-                        self.__schem.Blocks[x, z, y] = b_id
-                        self.__schem.Data[x, z, y] = b_state
+                        b_idn = int(map_block[b_id.lower()])
+                        if b_id.lower() in tileentities_list:
+                            if nbt_data == None:
+                                nbt_data = nbt.TAG_Compound()
+                                nbt_data.add(nbt.TAG_String(name='id', value=b_id.capitalize()))
+                            nbt_data.add(nbt.TAG_Int(name='x', value=x))
+                            nbt_data.add(nbt.TAG_Int(name='y', value=y))
+                            nbt_data.add(nbt.TAG_Int(name='z', value=z))
+                            self.__schem.TileEntities.append(nbt_data)
+                        self.__schem.Blocks[x, z, y] = b_idn
+                        self.__schem.Data[x, z, y] = verify_state(b_id, b_state)
                     else:
                         print 'BO3 Block not found: %s'%b_id
+
         else:
             log.error('Wrong type for \'filename\': got %s'%type(filename))
 
