8f3f71a6c0a1b7dae429a75d890a5000bde0c5b0
LaChal <denis.cuinat-guerraz@laposte.net>
2015-05-24 17:44:08 +0200

    Fix for paths not correctly created.

--- directories.py
+++ directories.py
@@ -368,6 +368,15 @@ sys.path.append(filtersDir)
 if not os.path.exists(getCacheDir()):
     os.makedirs(getCacheDir())
 
+# build the structures of directories if they don't exists
+for directory in (filtersDir, brushesDir, schematicsDir):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+bundledLibsDir = os.path.join(filtersDir, 'lib', 'Bundled Libraries')
+if not os.path.exists(bundledLibsDir):
+    os.makedirs(bundledLibsDir)
+
 # set userCachePath
 userCachePath = os.path.join(getCacheDir(),'usercache.json')
 # Make sure it exists
