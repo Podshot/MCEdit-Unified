fd4889112e66b6c47772ee202e8f87fdac46c9fa
TrazLander <jrayisom@gmail.com>
2015-05-23 23:44:58 -0400

    Added " - Copy" to Save As function
    Which un-reverts what it was before, which was adding "Copy of ".
    
    Don't judge me @naor2013 !
    
    At least doing it this way keeps your maps still grouped in an
    alphabetical order, since adding "Copy of" before the name would
    separate them.

--- leveleditor.py
+++ leveleditor.py
@@ -1241,8 +1241,8 @@ class LevelEditor(GLViewport):
     @mceutils.alertException
     def saveAs(self):
         shortName = os.path.split(os.path.split(self.level.filename)[0])[1]
-        filename = mcplatform.askSaveFile(directories.minecraftSaveFileDir, _("Name the new copy:"),
-                                          shortName, _('Minecraft World\0*.*\0\0'), "")
+        filename = mcplatform.askSaveFile(directories.minecraftSaveFileDir, _("Name the new copy."),
+                                          shortName + " - Copy", _('Minecraft World\0*.*\0\0'), "")
         if filename is None:
             return
         shutil.copytree(self.level.worldFolder.filename, filename)
