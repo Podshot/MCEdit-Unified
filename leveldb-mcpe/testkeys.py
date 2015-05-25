8e106c843a50475aa6433dbaa2a25bb312781425
Rubisk <rubiskmc@gmail.com>
2015-05-25 19:32:07 +0200

    Updated testkeys.py to display chunks instead of unreadable strings.

--- leveldb-mcpe/testkeys.py
+++ leveldb-mcpe/testkeys.py
@@ -1,5 +1,6 @@
 from leveldb_mcpe import DB, Options, ReadOptions, WriteOptions, WriteBatch
 import os
+import array
 
 op = Options()
 op.create_if_missing = True
@@ -11,7 +12,13 @@ with open('test.txt', 'w') as f:
     it = db.NewIterator(rop)
     it.SeekToFirst()
     while it.Valid():
-        f.write(it.key())
+        a = array.array("B", it.key())
+        xraw = a[0:4]
+        zraw = a[4:8]
+        chunkType = a[8]
+        x = sum(xraw[i] << (i*8) for i in range(len(xraw)))
+        z = sum(zraw[i] << (i*8) for i in range(len(zraw)))
+        f.write(str(zraw) + str(x) + ", " + str(z) + "| " + str(chunkType))
         f.write("\n")
         it.Next()
     it.status()  # Possible errors are handled by C++ side
