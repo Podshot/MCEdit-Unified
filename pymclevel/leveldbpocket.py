dd33d572e3674c4b05536da698a7058c2a01e449
Rubisk <rubiskmc@gmail.com>
2015-05-26 23:40:27 +0200

    Added chunk caching, deleting, and world closing.

--- pymclevel/leveldbpocket.py
+++ pymclevel/leveldbpocket.py
@@ -1,14 +1,15 @@
-from level import FakeChunk
+import itertools
+from level import FakeChunk, MCLevel
 import logging
 from materials import pocketMaterials
 import leveldb_mcpe
-from mclevelbase import ChunkNotPresent, notclosing
+from mclevelbase import ChunkNotPresent, ChunkMalformed, ChunkAccessDenied
 from nbt import TAG_List
 from numpy import array, fromstring, zeros
 import os
 import struct
-from infiniteworld import ChunkedLevelMixin
-from level import MCLevel, LightedChunk
+from infiniteworld import ChunkedLevelMixin, SessionLockLost
+from level import LightedChunk
 from contextlib import contextmanager
 
 logger = logging.getLogger(__name__)
@@ -59,18 +60,6 @@ class PocketLeveldbDatabase(object):
             with open(os.path.join(self.path, 'level.dat')) as f:
                 yield f
 
-    def close(self):
-        """
-        Should be called before deleting this instance of the level.
-        Not calling this method may result in corrupted worlds
-        :return: None
-        """
-        if PocketLeveldbDatabase.holdFileOpen:
-            del self._world_db
-            self._world_db = None
-            self._file.close()
-            self._file = None
-
     def __init__(self, path):
         """
         :param path: string, path to file
@@ -78,11 +67,9 @@ class PocketLeveldbDatabase(object):
         """
         self.path = path
 
-        try:
-            if not os.path.exists(path):
-                file(path, 'w').close()
-        except:
-            pass
+        if not os.path.exists(path):
+            file(path, 'w').close()
+
         self.options = leveldb_mcpe.Options()
         self.writeOptions = leveldb_mcpe.WriteOptions()
         self.readOptions = leveldb_mcpe.ReadOptions()
@@ -95,7 +82,21 @@ class PocketLeveldbDatabase(object):
             leveldb_mcpe.RepairWrapper(os.path.join(path, 'db'))
         # Maybe setup a logger message with number of chunks in the database etc.
 
-    def _readChunk(self, cx, cz, db=None, readOptions=None):
+    def close(self):
+        """
+        Should be called before deleting this instance of the level.
+        Not calling this method may result in corrupted worlds
+        :return: None
+        """
+        if PocketLeveldbDatabase.holdFileOpen:
+            if self._world_db is not None:
+                del self._world_db
+                self._world_db = None
+            if self._file is not None:
+                self._file.close()
+                self._file = None
+
+    def _readChunk(self, cx, cz, readOptions=None):
         """
         :param cx, cz: int Coordinates of the chunk
         :param db: DB
@@ -104,17 +105,20 @@ class PocketLeveldbDatabase(object):
         """
         key = struct.pack('<i', cx) + struct.pack('<i', cz) + "0"
         # print key
-        with (self.world_db() if db is None else db) as db:
+        with self.world_db() as db:
             rop = self.readOptions if readOptions is None else readOptions
             try:
                 data = db.Get(rop, key)
             except RuntimeError:
                 return None
-        assert (len(data) == 83200)
+
+        if len(data) != 83200:
+            raise ChunkMalformed(str(len(data)))
+
         logger.debug("CHUNK LOAD %s %s", cx, cz)
         return data
 
-    def saveChunk(self, chunk, db=None, writeOptions=None):
+    def saveChunk(self, chunk, batch=None, writeOptions=None):
         """
         :param chunk: PocketLeveldbChunk
         :param db: DB or WriteBatch
@@ -125,18 +129,21 @@ class PocketLeveldbDatabase(object):
         data = chunk.savedData()
         key = struct.pack('<i', cx) + struct.pack('<i', cz) + "0"
 
-        with (self.world_db() if db is None else db) as db:
-            wop = self.writeOptions if writeOptions is None else writeOptions
-            db.Put(key, data, wop)
+        if batch is None:
+            with self.world_db() as db:
+                wop = self.writeOptions if writeOptions is None else writeOptions
+                db.Put(key, data, wop)
+        else:
+            batch.Put(key, data)
 
-    def loadChunk(self, cx, cz, world, db=None):
+    def loadChunk(self, cx, cz, world):
         """
         :param cx, cz: int Coordinates of the chunk
         :param world: PocketLeveldbWorld
         :param db: DB
         :return: PocketLeveldbChunk
         """
-        data = self._readChunk(cx, cz, db=db)
+        data = self._readChunk(cx, cz)
         if data is None:
             raise ChunkNotPresent((cx, cz, self))
 
@@ -145,13 +152,25 @@ class PocketLeveldbDatabase(object):
 
     _allChunks = None
 
-    def getAllChunks(self, db=None, readOptions=None):
+    def deleteChunk(self, cx, cz, batch=None, writeOptions=None):
+        if batch is None:
+            with self.world_db() as db:
+                key = struct.pack('<i', cx) + struct.pack('<i', cz) + "0"
+                wop = self.writeOptions if writeOptions is None else writeOptions
+                db.Delete(wop, key)
+        else:
+            key = struct.pack('<i', cx) + struct.pack('<i', cz) + "0"
+            batch.Delete(key)
+
+        logger.debug("DELETED CHUNK %s %s", cx, cz)
+
+    def getAllChunks(self, readOptions=None):
         """
         :param db: DB
         :param readOptions: ReadOptions
         :return: list
         """
-        with (self.world_db() if db is None else db) as db:
+        with self.world_db() as db:
             allChunks = []
             rop = self.readOptions if readOptions is None else readOptions
 
@@ -165,14 +184,10 @@ class PocketLeveldbDatabase(object):
 
                 if t == "0":
                     cx, cz = struct.unpack('<i', rawx), struct.unpack('<i', rawz)
-                    try:
-                        db.Get(rop, key)
-                    except RuntimeError:
-                        print "iteration invalid"
-                    allChunks.append([cx[0], cz[0]])
+                    allChunks.append((cx[0], cz[0]))
                 it.Next()
+            it.status()  # All this does is cause an exception if something went wrong. Might be unneeded?
             del it
-            # it.status()  # All this does is cause an exception if something went wrong. Might be unneeded?
             return allChunks
 
 class PocketLeveldbWorld(ChunkedLevelMixin, MCLevel):
@@ -183,25 +198,77 @@ class PocketLeveldbWorld(ChunkedLevelMixin, MCLevel):
     isInfinite = True
     materials = pocketMaterials
 
+    _allChunks = None  # An array of cx, cz pairs.
+    _loadedChunks = {}  # A dictionary of actual PocketLeveldbChunk objects mapped by (cx, cz)
+
     @property
     def allChunks(self):
-        return self.chunkFile.getAllChunks()
+        if self._allChunks is None:
+            self._allChunks = self.chunkFile.getAllChunks()
+        return self._allChunks
 
     def __init__(self, filename):
         if not os.path.isdir(filename):
             filename = os.path.dirname(filename)
         self.filename = filename
-        self.dimensions = {}
 
         self.chunkFile = PocketLeveldbDatabase(os.path.join(filename, 'db'))
-        self._loadedChunks = {}
 
     def getChunk(self, cx, cz):
         c = self._loadedChunks.get((cx, cz))
         if c is None:
             c = self.chunkFile.loadChunk(cx, cz, self)
-            self._loadedChunks[cx, cz] = c
+            self._loadedChunks[(cx, cz)] = c
         return c
+
+    def unload(self):
+        """
+        Unload all chunks and close all open file-handlers.
+        """
+        self._loadedChunks.clear()
+        self._allChunks = None
+        self.chunkFile.close()
+
+    def close(self):
+        """
+        Unload all chunks and close all open file-handlers. Discard any unsaved data.
+        """
+        self.unload()
+        try:
+            pass  # Setup a way to close a work-folder?
+        except SessionLockLost:
+            pass
+
+    def deleteChunk(self, cx, cz, batch=None):
+        self.chunkFile.deleteChunk(cx, cz, batch=batch)
+        if self._loadedChunks is not None and (cx, cz) in self._loadedChunks:  # Unnecessary check?
+            del self._loadedChunks[(cx, cz)]
+            self.allChunks.remove((cx, cz))
+
+    def deleteChunksInBox(self, box):
+        logger.info(u"Deleting {0} chunks in {1}".format((box.maxcx - box.mincx) * (box.maxcz - box.mincz),
+                                                         ((box.mincx, box.mincz), (box.maxcx, box.maxcz))))
+        i = 0
+        ret = []
+        batch = leveldb_mcpe.WriteBatch()
+        print 'test'
+        for cx, cz in itertools.product(xrange(box.mincx, box.maxcx), xrange(box.mincz, box.maxcz)):
+            i += 1
+            if self.containsChunk(cx, cz):
+                self.deleteChunk(cx, cz, batch=batch)
+                ret.append((cx, cz))
+
+            assert not self.containsChunk(cx, cz), "Just deleted {0} but it didn't take".format((cx, cz))
+
+            if i % 100 == 0:
+                logger.info(u"Chunk {0}...".format(i))
+
+        with self.chunkFile.world_db() as db:
+            wop = self.chunkFile.writeOptions
+            db.Write(wop, batch)
+
+        del batch
+        return ret
 
     @classmethod
     def _isLevel(cls, filename):
@@ -219,16 +286,16 @@ class PocketLeveldbWorld(ChunkedLevelMixin, MCLevel):
         batch = leveldb_mcpe.WriteBatch()
         for chunk in self._loadedChunks.itervalues():
             if chunk.dirty:
-                self.chunkFile.saveChunk(chunk, db=batch)
+                self.chunkFile.saveChunk(chunk, batch=batch)
                 chunk.dirty = False
             yield
 
-        with self.chunkFile.world_db as db:
+        with self.chunkFile.world_db() as db:
             wop = self.chunkFile.writeOptions
             db.Write(wop, batch)
 
     def containsChunk(self, cx, cz):
-        return True
+        return (cx, cz) in self.allChunks
 
     @property
     def chunksNeedingLighting(self):
@@ -253,7 +320,7 @@ class PocketLeveldbChunk(LightedChunk):
         self.SkyLight, data = data[:16384], data[16384:]
         self.BlockLight, data = data[:16384], data[16384:]
         self.DirtyColumns, data = data[:256], data[256:]
-        self.GrassColors = data[1024:]  # Unused at the moment. Might need a special editor? Maybe hooked up to biomes?
+        self.GrassColors = data[:1024]  # Unused at the moment. Might need a special editor? Maybe hooked up to biomes?
 
         self.unpackChunkData()
         self.shapeChunkData()
@@ -305,4 +372,5 @@ class PocketLeveldbChunk(LightedChunk):
                         packData(self.SkyLight).tostring(),
                         packData(self.BlockLight).tostring(),
                         self.DirtyColumns.tostring(),
+                        self.GrassColors.tostring(),
                         ])
