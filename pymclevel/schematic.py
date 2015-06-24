860c14274d1aef020d513939c01dc9220755226f
naor2013 <naor2013@icloud.com>
2015-06-24 16:05:48 +0300

    Doesn't have an error if a tag is missing

--- pymclevel/schematic.py
+++ pymclevel/schematic.py
@@ -477,41 +477,53 @@ class MCSchematic(EntityLevel):
         log.info(u"E/W Flip: Relocating entities...")
         for entity in self.Entities:
 
-            entity["Pos"][2].value = self.Length - entity["Pos"][2].value
-            entity["Motion"][2].value = -entity["Motion"][2].value
+            try:
+                entity["Pos"][2].value = self.Length - entity["Pos"][2].value
+            except:
+                pass
+            try:
+                entity["Motion"][2].value = -entity["Motion"][2].value
+            except:
+                pass
 
-            entity["Rotation"][0].value -= 180.0
+            try:
+                entity["Rotation"][0].value -= 180.0
+            except:
+                pass
 
             # Special logic for old width painting as TileX/TileZ favours -x/-z
 
-            if entity["id"].value in ("Painting", "ItemFrame"):
-                facing = entity.get("Facing", entity.get("Direction"))
-                if facing is None:
-                    dirFacing = entity.get("Dir")
-                    if dirFacing is not None:
-                        if dirFacing.value == 0:
-                            dirFacing.value = 2
-                        elif dirFacing.value == 2:
-                            dirFacing.value = 0
-                        facing = dirFacing
-                    else:
-                        raise Exception("None of tags Facing/Direction/Dir found in entity %s during flipping -  %r" % (entity["id"].value, entity))
+            try:
+                if entity["id"].value in ("Painting", "ItemFrame"):
+                    facing = entity.get("Facing", entity.get("Direction"))
+                    if facing is None:
+                        dirFacing = entity.get("Dir")
+                        if dirFacing is not None:
+                            if dirFacing.value == 0:
+                                dirFacing.value = 2
+                            elif dirFacing.value == 2:
+                                dirFacing.value = 0
+                            facing = dirFacing
+                        else:
+                            raise Exception("None of tags Facing/Direction/Dir found in entity %s during flipping -  %r" % (entity["id"].value, entity))
 
-            if entity["id"].value == "Painting":
-                if facing.value == 1:
-                    entity["TileZ"].value = self.Length - entity["TileZ"].value - 2 + self.paintingMap[entity["Motive"].value] % 2
-                elif facing.value == 3:
-                    entity["TileZ"].value = self.Length - entity["TileZ"].value - self.paintingMap[entity["Motive"].value] % 2
-                else:
+                if entity["id"].value == "Painting":
+                    if facing.value == 1:
+                        entity["TileZ"].value = self.Length - entity["TileZ"].value - 2 + self.paintingMap[entity["Motive"].value] % 2
+                    elif facing.value == 3:
+                        entity["TileZ"].value = self.Length - entity["TileZ"].value - self.paintingMap[entity["Motive"].value] % 2
+                    else:
+                        entity["TileZ"].value = self.Length - entity["TileZ"].value - 1
+                    if facing.value == 0:
+                        entity["TileX"].value = entity["TileX"].value + 1 - self.paintingMap[entity["Motive"].value] % 2
+                    elif facing.value == 2:
+                        entity["TileX"].value = entity["TileX"].value - 1 + self.paintingMap[entity["Motive"].value] % 2
+                    facing.value = eastWestPaintingMap[facing.value]
+                elif entity["id"].value == "ItemFrame":
                     entity["TileZ"].value = self.Length - entity["TileZ"].value - 1
-                if facing.value == 0:
-                    entity["TileX"].value = entity["TileX"].value + 1 - self.paintingMap[entity["Motive"].value] % 2
-                elif facing.value == 2:
-                    entity["TileX"].value = entity["TileX"].value - 1 + self.paintingMap[entity["Motive"].value] % 2
-                facing.value = eastWestPaintingMap[facing.value]
-            elif entity["id"].value == "ItemFrame":
-                entity["TileZ"].value = self.Length - entity["TileZ"].value - 1
-                facing.value = eastWestPaintingMap[facing.value]
+                    facing.value = eastWestPaintingMap[facing.value]
+            except:
+                pass
 
         for tileEntity in self.TileEntities:
             tileEntity["z"].value = self.Length - tileEntity["z"].value - 1
