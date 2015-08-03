f3199756eb02e4dfbf0f1bea55581d926ac9d7f0
Rubisk <rubiskmc@gmail.com>
2015-08-03 16:45:57 +0200

    Fixed PE support

---
+++ MCWorldLibrary/leveldb_mcpe/__init__.py
@@ -0,0 +1,8 @@
+# We got a kinda wonky setup here.
+# Unfortunately, I named both this package leveldb_mcpe and
+# the wrapper leveldb_mcpe.
+# By making this a proper package and making the wrapper a member
+# of the package, it at least works, although it is kinda wonky.
+
+# Maybe someday, rename one of the 2 and sort out all the issues.
+import leveldb_mcpe
