49a773424f8ae393c318ab091d59c1830f86c2e1
LaChal <denis.cuinat-guerraz@laposte.net>
2016-01-09 14:39:45 +0100

    Changed the editor top bar layout.
    These changes make this bar 'shorter', and allow 'Waypoints' button to be visible (event cut) on the smallest display size (1000x680) for all languages:
    
    * 'MCEdit' button renamed 'Menu'.
    * View distances arrows changed for a choice button.
    * 'Record Undo' label changed for 'Undo'.
    
    German and Nederlands translation make the waypoint cut becayse of the length of the 'Undo' translation. May be shortened a bit?

--- leveleditor.py
+++ leveleditor.py
@@ -211,10 +211,14 @@ class LevelEditor(GLViewport):
 
         self.optionsBar = Widget()
 
-        self.mcEditButton = Button("MCEdit", action=self.showControls)
-        self.viewDistanceDown = Button("<", action=self.decreaseViewDistance)
-        self.viewDistanceUp = Button(">", action=self.increaseViewDistance)
-        self.viewDistanceReadout = ValueDisplay(width=40, ref=AttrRef(self.renderer, "viewDistance"))
+        self.mcEditButton = Button("Menu", action=self.showControls)
+
+        def chooseDistance():
+            self.changeViewDistance(int(self.viewDistanceReadout.get_value()))
+        self.viewDistanceReadout = ChoiceButton(["%s"%a for a in range(2,34,2)], width=20, ref=AttrRef(self.renderer, "viewDistance"), choose=chooseDistance)
+        self.viewDistanceReadout.selectedChoice = "%s"%self.renderer.viewDistance
+        self.viewDistanceReadout.shrink_wrap()
+
 
         def showViewOptions():
             col = [CheckBoxLabel("Entities", fg_color=(0xff, 0x22, 0x22),
@@ -258,7 +262,7 @@ class LevelEditor(GLViewport):
         self.viewportButton = Button("Camera View", action=self.swapViewports,
                                      tooltipText=_("Shortcut: {0}").format(_(config.keys.toggleView.get())))
 
-        self.recordUndoButton = CheckBoxLabel("Record Undo", ref=AttrRef(self, 'recordUndo'))
+        self.recordUndoButton = CheckBoxLabel("Undo", ref=AttrRef(self, 'recordUndo'))
 
         # TODO: Mark
         self.sessionLockLock = Image(image.load(open(directories.getDataDir(os.path.join(u"toolicons",
@@ -270,9 +274,10 @@ class LevelEditor(GLViewport):
         self.sessionLockLabel.mouse_down = self.mouse_down_session
         
         # TODO: Marker
-        row = (self.mcEditButton, self.viewDistanceDown, Label("View Distance:"), self.viewDistanceReadout,
-               self.viewDistanceUp, self.viewButton, self.viewportButton, self.recordUndoButton,
+        row = (self.mcEditButton, Label("View Distance:"), self.viewDistanceReadout,
+               self.viewButton, self.viewportButton, self.recordUndoButton,
                Row((self.sessionLockLabel, self.sessionLockLock), spacing=2), self.waypointsButton)
+
 
         self.topRow = row = Row(row)
         self.add(row)
@@ -316,7 +321,6 @@ class LevelEditor(GLViewport):
         GLViewport.set_update_translation(self, v)
         if v:
             self.statusLabel.width = self.width
-            self.viewDistanceReadout.width = 40
             self.topRow.calc_size()
             self.controlPanel.set_update_translation(v)
     #-#
@@ -1227,8 +1231,8 @@ class LevelEditor(GLViewport):
             self.netherButton.selectedChoice = [d[0] for d in dimensionsMenu if d[1] == str(self.level.dimNo)][0]
             self.remove(self.topRow)
             # TODO: Marker
-            self.topRow = Row((self.mcEditButton, self.viewDistanceDown, Label("View Distance:"),
-                               self.viewDistanceReadout, self.viewDistanceUp, self.viewButton,
+            self.topRow = Row((self.mcEditButton, Label("View Distance:"),
+                               self.viewDistanceReadout, self.viewButton,
                                self.viewportButton, self.recordUndoButton, self.netherButton,
                                Row((self.sessionLockLabel, self.sessionLockLock), spacing=2), self.waypointsButton))
             self.add(self.topRow, 0)
@@ -2445,13 +2449,8 @@ class LevelEditor(GLViewport):
         self.addWorker(self.renderer)
         config.settings.viewDistance.set(self.renderer.viewDistance)
 
-    def increaseViewDistance(self):
-        self.renderer.viewDistance = min(self.renderer.maxViewDistance, self.renderer.viewDistance + 2)
-        self.addWorker(self.renderer)
-        config.settings.viewDistance.set(self.renderer.viewDistance)
-
-    def decreaseViewDistance(self):
-        self.renderer.viewDistance = max(self.renderer.minViewDistance, self.renderer.viewDistance - 2)
+    def changeViewDistance(self, dist):
+        self.renderer.viewDistance = min(self.renderer.maxViewDistance, dist)
         self.addWorker(self.renderer)
         config.settings.viewDistance.set(self.renderer.viewDistance)
 
