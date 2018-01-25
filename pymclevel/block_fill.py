37e3c1c9cbc9eb8d44e5a7babb652910f09240c8
Podshot <ben.gothard3@gmail.com>
2018-01-25 09:02:55 -0500

    Tile Entities now use their string ids for the "id" tag, fixes #897

--- pymclevel/block_fill.py
+++ pymclevel/block_fill.py
@@ -50,8 +50,12 @@ def fillBlocksIter(level, box, blockInfo, blocksToReplace=(), noData=False):
                 changesLighting = True
 
     tileEntity = None
-    if blockInfo.stringID in TileEntity.stringNames.keys():
-        tileEntity = TileEntity.stringNames[blockInfo.stringID]
+    split_ver = level.gameVersion.split('.')
+    if int(split_ver[0]) >= 1 and int(split_ver[1]) >= 11:
+        tileEntity = "minecraft:{}".format(blockInfo.stringID)
+    else:
+        if blockInfo.stringID in TileEntity.stringNames.keys():
+            tileEntity = TileEntity.stringNames[blockInfo.stringID]
 
     blocksIdToReplace = [block.ID for block in blocksToReplace]
 
