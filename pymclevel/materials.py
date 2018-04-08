3a5c14bb9b5e8ac631a5773aa50a7c7761a8c5ad
Naor Volkovich <naor2013@icloud.com>
2018-04-08 19:23:04 +0300

    Revert "Fix issue with missing blocks in PE 1.2.13"
    This reverts commit 6199e67d91f481ef12065086a88716f80a8c9fbe.
    
    # Conflicts:
    #	pymclevel/leveldbpocket.py

--- pymclevel/materials.py
+++ pymclevel/materials.py
@@ -303,17 +303,13 @@ class MCMaterials(object):
             for b in self.allBlocks:
                 if b.name.lower() == key:
                     return b
-                idStr = b.idStr
-                if not isinstance(idStr, list):
-                    idStr = [idStr]
-                for blockStr in idStr:
-                    if blockStr.lower() == key:
-                        if b.blockData == 0:
-                            return b
-                        elif not lowest_block:
+                if b.idStr.lower() == key:
+                    if b.blockData == 0:
+                        return b
+                    elif not lowest_block:
+                        lowest_block = b
+                    elif lowest_block.blockData > b.blockData:
                             lowest_block = b
-                        elif lowest_block.blockData > b.blockData:
-                                lowest_block = b
             if lowest_block:
                 return lowest_block
 
@@ -1375,13 +1371,7 @@ if '--find-blockstates' in os.sys.argv:
         ID = block.ID
         DATA = block.blockData
         pc_block = alphaMaterials.get((ID, DATA))
-        pc_block_string = pc_block.stringID
-        pe_block_string = block.stringID
-        if not isinstance(pc_block_string, list):
-            pc_block_string = [pc_block_string]
-        if not isinstance(pe_block_string, list):
-            pe_block_string = [pe_block_string]
-        if not set(pc_block_string).isdisjoint(pe_block_string):  # Checks if the lists has an element in common
+        if pc_block and pc_block.stringID == block.stringID:
             passed.append(block)
         else:
             failed.append(block)
